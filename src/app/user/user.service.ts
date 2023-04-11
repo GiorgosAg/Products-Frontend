ed8d827c8cfdf07eba42808792ffc69c4bcb21dc
GiorgosAg <GiorgosAg>
2023-04-11 19:48:54 +0300

    Update

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserAPIList } from './user.interfaces';
+import { delay } from 'rxjs';
 
 const USER_API = 'https://codingfactory.ddns.net/api/user';
 
@@ -9,6 +10,6 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   findAll() {
-    return this.http.get<UserAPIList>(`${USER_API}/findall`);
+    return this.http.get<UserAPIList>(`${USER_API}/findall`).pipe(delay(1000));
   }
 }
--- src/app/user/users-list/users-list.component.ts
+++ src/app/user/users-list/users-list.component.ts
@@ -9,17 +9,23 @@ import { UserAPIList } from '../user.interfaces';
 })
 export class UsersListComponent implements OnInit {
   constructor(private userService: UserService) {}
+
+  loading = false;
+
   ngOnInit(): void {
     console.log('Starting "findall" API call');
+    this.loading = true;
     this.userService.findAll().subscribe({
       next: (apiData: UserAPIList) => {
         const { status, data } = apiData;
         console.log(status, data);
       },
       error: (error) => {
+        this.loading = false;
         console.log(error);
       },
       complete: () => {
+        this.loading = false;
         console.log('API call completed');
       },
     });
